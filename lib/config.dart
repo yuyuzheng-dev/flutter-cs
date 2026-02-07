5d0778c26c59aca221b709c6b5c9f38838ecb9a4
fockau <240287296+fockau@users.noreply.github.com>
2026-02-08 01:57:46 +0800

    Create config.dart

---
+++ lib/config.dart
@@ -0,0 +1,12 @@
+class AppConfig {
+  /// 网站地址（给你展示用 / 打开网页用）
+  /// 你说“网站默认带 /api/v1”
+  static const String siteBaseUrl = 'https://com.android22.com/api/v1';
+
+  /// API 地址（不带 /api/v1）
+  static const String apiBaseUrl = 'https://com.android22.com';
+
+  /// 可选：固定 Header（如果你后面想要，先留着）
+  /// static const String hasiHeaderName = 'hasi';
+  /// static const String hasiHeaderValue = '18df203a-6abf-41ff-b10c-0be7b00f59a6';
+}
